e70a3adf684592df5f831750018c44f05df7e4d0
Pratik Talreja <talreja9191@gmail.com>
2021-12-01 08:44:12 -0500

    Inital code

---
+++ src/Business/DeliveryMan/DeliveryMan.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.DeliveryMan;
+
+import java.util.ArrayList;
+import Business.Order.Order;
+
+/**
+ *
+ * @author harold
+ */
+public class DeliveryMan {
+    private String name;
+    private String address;
+    private String email;
+    private String phoneNumber;
+    private int age;
+    private ArrayList<Order> orderList;
+    
+    public DeliveryMan(String name, String address, String email, String phoneNumber, int age) {
+        this.name = name;
+        this.address = address;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        orderList = new ArrayList<Order>();
+    }
+    
+    /*public void addOrder(String restaurantName, String customerName, String deliverMan, ArrayList<Dishes> Order, String cost, String deliveryAddress) {
+        Order order=new Order();
+        
+        order.setCustomerName(customerName);
+        order.setRestaurantName(restaurantName);
+        order.setDeliveryMan(deliverMan);
+        order.setOrder(Order);
+        order.setPrice(cost);
+        order.setDeliveryAddress(deliveryAddress);
+        order.setOrderStatus("New Order");
+        orderList.add(order);
+        
+    }*/
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    @Override
+    public String toString() {
+        return name;
+    }
+    
+    
+}
